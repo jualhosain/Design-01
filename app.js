618b9070211e0005a0a638a7cb3f2e48f625d59b
Md Jual Hosain <jualhosain@gmail.com>
2020-07-26 14:29:58 +0600

    New Design for Protfolio

---
+++ app.js
@@ -0,0 +1,6 @@
+let burger = document.querySelector('.burger');
+let mobileMenu = document.querySelector('.mobileMenu');
+burger.addEventListener('click',()=>{
+	burger.classList.toggle('active')
+	mobileMenu.classList.toggle('active');
+});
